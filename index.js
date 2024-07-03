167b67a4e0c6223d9ec698da6d9e21295694f814
avant muchiri <avant.muchiri@student.moringaschool>
2024-07-03 12:12:59 +0300

    strings lab

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-const currentUser = 'AVANT BMW';
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const currentUser = 'AVANT';
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
